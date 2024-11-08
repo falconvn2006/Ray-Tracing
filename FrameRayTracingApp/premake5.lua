a2eba2ed4eea4694015c098e14f6c289d4e4c1a6
falcondev <tinhtcp@gmail.com>
2024-11-07 19:52:10 -0700

    Change some files name

--- FrameRayTracingApp/premake5.lua
+++ FrameRayTracingApp/premake5.lua
@@ -1,4 +1,4 @@
-project "App"
+project "FrameRayTracingApp"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,9 @@
-workspace "Frame"
+workspace "FrameRayTracing"
     architecture "x64"
     configurations { "Debug", "Release", "Dist" }
-    startproject "App"
+    startproject "FrameRayTracingApp"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 include "VisualStudioConfig.lua"
-include "App"
+include "FrameRayTracingApp"
