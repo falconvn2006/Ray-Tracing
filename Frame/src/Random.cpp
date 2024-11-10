ebfe18ecb058b9f8579cd21fd7b9290567158c9e
falcondev <tinhtcp@gmail.com>
2024-11-10 15:33:19 -0700

    Optimize the random

--- Frame/src/Random.cpp
+++ Frame/src/Random.cpp
@@ -2,6 +2,6 @@
 
 namespace Frame
 {
-	std::mt19937 Random::s_RandomEngine;
+	thread_local std::mt19937 Random::s_RandomEngine;
 	std::uniform_int_distribution<std::mt19937::result_type> Random::s_Distribution;
 }
--- Frame/src/Random.h
+++ Frame/src/Random.h
@@ -45,7 +45,7 @@ namespace Frame
 		}
 
 	private:
-		static std::mt19937 s_RandomEngine;
+		static thread_local std::mt19937 s_RandomEngine;
 		static std::uniform_int_distribution<std::mt19937::result_type> s_Distribution;
 	};
 }
